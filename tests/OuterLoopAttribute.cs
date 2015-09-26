ef5269fc71b950e0d7cdece9b0c69ecd5ffe179d
Andrea Martinelli <martinelli-andrea@outlook.com>
2015-09-26 20:34:50 +0200

    initial commit

---
+++ tests/OuterLoopAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Linq.Tests
+{
+    internal class OuterLoopAttribute : Attribute
+    {
+    }
+    internal class ActiveIssue : Attribute
+    {
+        public ActiveIssue(int id) { }
+    }
+}
